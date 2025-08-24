434efb9e86aff115271eb77c90fb6639397e9896
Elsa Zougouri <elsazougouri224@gmail.com>
2025-08-24 02:51:56 +0000

    fix: Comment caching to test fix cors issue

--- user-backend/build-app.sh
+++ user-backend/build-app.sh
@@ -14,7 +14,7 @@ php artisan config:clear
 php artisan route:clear
 
 # Cache the various components of the Laravel application
-php artisan config:cache
-php artisan event:cache
-php artisan route:cache
-php artisan view:cache
+# php artisan config:cache
+# php artisan event:cache
+# php artisan route:cache
+# php artisan view:cache
